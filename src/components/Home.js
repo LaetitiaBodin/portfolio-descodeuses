91669a6a6f7593b17a8346f8a1482d57a377a8a9
LaetitiaBodin <laetitia.bodin.webdev@gmail.com>
2021-11-27 18:50:25 +0100

    feat: set Home

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+export const Home = () => {
+    return (
+        <div className='home'>
+            <h1>PORTFOLIO @DESCODEUSES</h1>
+            <div className='home_card_row'>
+                <LinkCard {...{page: 'html-css', name: 'HTML / CSS'}}/>
+                <LinkCard {...{page: 'javascript', name: 'JAVASCRIPT'}}/>
+                <LinkCard {...{page: 'php', name: 'PHP'}}/>
+                <LinkCard {...{page: 'projets', name: 'PROJETS'}}/>
+            </div>
+        </div>
+    )
+}
+
+const LinkCard = ({page, name}) => {
+    return <Link to={page} className='home_card_block'>{name}</Link>
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { Home } from './components/Home'
 
 import './index.css'
 
 const App = () => {
-    return <></>
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/html-css'>
+                    HTML/CSS
+                </Route>
+                <Route exact path='/javascript'>
+                    JAVASCRIPT
+                </Route>
+                <Route exact path='/php'>
+                    PHP
+                </Route>
+                <Route exact path='/projets'>
+                    PROJETS
+                </Route>
+                <Route exact path='*'>
+                    ERREUR
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
